671a1caeb9b565a0826a5ad11add6022fa025689
Greenwalt <KG155781@davenportschools.org>
2019-04-23 13:20:45 -0500

    update

--- src/MetricConversion.java
+++ src/MetricConversion.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class MetricConversion {
     public static void main(String[] args) {
-        
+        double centimeter;
+        double liters;
+        double inches;
+        double gallon;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of centimeters you have");
+
 
     }
 }
